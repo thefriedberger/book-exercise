f7abaeb0d3bf2f7ec8469b1f3b1242ae1dd36465
Tyler Friedberg <thefriedberger@gmail.com>
2016-09-23 14:47:36 -0400

    Finished?
    It should be good to go as long as the commits actually take this time.

--- Book.java
+++ Book.java
@@ -11,16 +11,90 @@ class Book
     // The fields.
     private String author;
     private String title;
+    private int pages;
+    private String refNumber;
+    private int borrowed;
+    private boolean courseText;
 
     /**
      * Set the author and title fields when this object
      * is constructed.
      */
-    public Book(String bookAuthor, String bookTitle)
+    public Book(String bookAuthor, String bookTitle, int pageNum, int borrow)
     {
         author = bookAuthor;
         title = bookTitle;
+        pages = pageNum;
+        refNumber = "";
+        borrowed = borrow;
+        courseText = true;
     }
+    
+    public String getAuthor()
+    {
+        return author;
+    }
+    
+    public String getTitle()
+    {
+        return title;
+    }
+    
+    public int getPages()
+    {
+        return pages;
+    }
+    
+       public String getRefNumber()
+    {
+        return refNumber;
+    }
+    
+    public void setRefNumber(String ref)
+    {
+        if (ref.length() == 3)
+        {
+             refNumber = ref;
+        }
+        else
+        {
+            System.out.println("You did something wrong.");
+        }
+    }
+    
+    public int getBorrowed()
+    {
+        borrowed = borrowed + 1;
+        return borrowed;
+    }
+    
+    public boolean isCourseText()
+    {
+        return courseText;
+    }
+    
+    public void printAuthor()
+    {
+        System.out.println("The author of the book is " + author);
+    }
+    
+    public void printTitle()
+    {
+        System.out.println("The title of the book is " + title);
+    }
+    
+    public void printDetails()
+    {
+        if (refNumber.length() == 0)
+        {
+            System.out.println("Author: " + author + " . Title: " + title + " . Pages: " + pages + " . Reference number: 'ZZZ'" + " The book has been borrowed " + borrowed + "times");
+        }
+        else
+        {
+            System.out.println("Author: " + author + " . Title: " + title + " . Pages: " + pages + " . Reference number: " + refNumber + " The book has been borrowed " + borrowed + " time(s)");
+        }
+    }
+        
 
     // Add the methods here ...
 }
